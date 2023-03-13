68710680acca3543761baf4f8577213d48141a56
Shubham Maheshwari <77975234+shubhamghb@users.noreply.github.com>
2023-03-14 04:10:12 +0530

    Time: 30 ms (48.54%), Space: 10.1 MB (42.56%) - LeetHub

---
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int maxConsecutiveAnswers(string s, int k) {
+        int ans=0;
+        int i=0,l=0,cnt=0;
+        while(i<s.size()){
+            if(s[i]=='T')
+                cnt++;
+ 
+            while(cnt>k){
+                if(s[l]=='T')
+                    cnt--;
+                l++;
+            }
+            ans=max(ans,i-l+1);
+            i++;
+        }
+ 
+        cnt=0,l=0,i=0;
+        while(i<s.size()){
+            if(s[i]=='F')
+                cnt++;
+            while(cnt>k){
+                if(s[l]=='F')
+                    cnt--;
+                l++;
+            } 
+            ans=max(ans,i-l+1);
+            i++;
+        }
+ 
+        return ans;
+    }
+};
